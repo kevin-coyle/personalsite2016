bde5874291b8d8710c767d8665cdf0fac3af8cb9
Kevin Coyle <kevin.coyle@drutek.co.uk>
2016-05-01 23:53:44 +0100

    Moved hash to env var

--- app/settings/settings.php
+++ app/settings/settings.php
@@ -293,7 +293,8 @@ $config_directories = array();
  *   $settings['hash_salt'] = file_get_contents('/home/example/salt.txt');
  * @endcode
  */
-$settings['hash_salt'] = '';
+ define("HASH") = getenv("HASH");
+$settings['hash_salt'] = HASH;
 
 /**
  * Deployment identifier.
